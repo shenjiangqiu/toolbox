8e69733d27223f8e8b088935e255fc184c6c8ada
Jiangqiu Shen <ivy22233qiu@live.com>
2024-06-17 21:10:45 -0400

    update

--- src/bin/move_all_torrents.rs
+++ src/bin/move_all_torrents.rs
@@ -1,8 +1,18 @@
+use std::io::BufReader;
+
+use clap::Parser;
 use toolbox::TorrentInfo;
+#[derive(Parser)]
+struct Cli {
+    #[clap(short, long)]
+    dry_run: bool,
+}
 fn main() {
+    let cli = Cli::parse();
     let info: Vec<TorrentInfo> =
-        bincode::deserialize_from(std::fs::File::open("info.bin").unwrap()).unwrap();
-    let dest = std::path::PathBuf::from("/home/sjq/Downloads/qbt/");
+        bincode::deserialize_from(BufReader::new(std::fs::File::open("info.bin").unwrap()))
+            .unwrap();
+    let dest = std::path::PathBuf::from("/home/sjq/usb/Downloads/qbt/");
     for t in info {
         let category = t.info.category;
         let from = std::path::PathBuf::from(t.info.content_path.unwrap());
@@ -18,6 +28,10 @@ fn main() {
             }
             None => dest.join(t.info.name.unwrap()),
         };
-        std::fs::rename(from, to).unwrap();
+        if cli.dry_run {
+            println!("from: {:?}, to: {:?}", from, to);
+        } else {
+            std::fs::rename(from, to).unwrap();
+        }
     }
 }
